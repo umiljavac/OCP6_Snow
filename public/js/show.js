c0a73d73d9c0b470c6d107f4083b1e90f9742edc
ulrich <ulm_ulm@hotmail.com>
2018-02-06 12:15:53 +0100

    Media button added for small screen

--- public/js/show.js
+++ public/js/show.js
@@ -1,5 +1,7 @@
+var imageZoom = 0;
 $(document).ready(function () {
     $('.image').click(function () {
+        imageZoom = 1;
         var imgClone = $(this).clone();
         $(".flex-div").hide();
         imgClone.addClass("image-full");
@@ -8,10 +10,44 @@ $(document).ready(function () {
         $('#trick-title').append(zoomDiv);
 
         $(imgClone).click(function () {
+            imageZoom = 0;
             zoomDiv.remove();
             $(".flex-div").show();
             centerBlocks();
         });
     });
-});
 
+    checkSize();
+    $(window).resize(function() {
+
+        checkSize();
+    });
+
+    function checkSize() {
+        var medias = $('#flex-show');
+        var openMedias = $('#view-media');
+        var winW = window.innerWidth;
+
+        if (winW < 460) {
+            if (imageZoom === 0) {
+                medias.css('display', 'none');
+                openMedias.css('display', 'flex');
+                openMedias.on('click', function () {
+                    $(this).hide();
+                    medias.css('display', 'flex');
+                    centerBlocks();
+                })
+            }
+        } else {
+            openMedias.css('display', 'none');
+            if (imageZoom === 0) {
+                medias.css('display', 'flex');
+            } else {
+                medias.css('display', 'none');
+            }
+        }
+    }
+
+    var letters = $('.trick-name h1').text();
+    console.log(letters);
+});
