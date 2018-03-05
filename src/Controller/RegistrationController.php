7767d8c415f34fc59d764883b519381e5f36a52d
ulrich <ulm_ulm@hotmail.com>
2018-03-05 13:17:57 +0100

    files of /src folder comply with PSR2 standards

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,80 +8,45 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\Type\UserType;
+use App\Service\EntityManager\UserManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends Controller
 {
     /**
      * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param UserManager $userManager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      * @Route("/register", name="user_registration")
      */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer)
+    public function registerAction(Request $request, UserManager $userManager)
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
-            $activationToken = md5($user->getEmail());
-            $user->setActivationToken($activationToken);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-            $this->sendConfirmation($mailer, $user, $activationToken);
-            $this->addFlash('login', 'Un email a été envoyé dans ta boîte mail, merci de cliquer sur le lien qu\'il contient pour finaliser ton inscription !');
+        $managerResponse = $userManager->registerUser($request);
+        if ($managerResponse === true) {
+            $this->addFlash('login', 'Un email a été envoyé dans ta boîte mail, 
+                                           merci de cliquer sur le lien qu\'il contient 
+                                           pour finaliser ton inscription !');
             return $this->redirectToRoute('home');
         }
-        return $this->render(
-            'views/register.html.twig',
-            array('form' => $form->createView())
-        );
+        return $this->render('views/register.html.twig', array('form' => $managerResponse));
     }
 
     /**
-     * @param Request $request
+     * @param UserManager $userManager
+     * @param $token
+     * @return mixed
      * @Route("/register/{token}/validation", name="register_validation")
      */
-    public function registerValidationAction($token)
+    public function registerValidationAction($token, UserManager $userManager)
     {
-        $repository = $this->getDoctrine()->getManager()->getRepository(User::class);
-        $user = $repository->findOneBy(['activationToken' => $token]);
-        $em = $this->getDoctrine()->getManager();
+        $userManager->userAccountValidation($token);
 
-        if (!$user) {
-            throw $this->createNotFoundException('Désolé c\'est moche : tu n\'éxiste pas :( Try again !');
-        }
-        $user->setActive(true);
-        $user->setActivationToken(null);
-        $em->persist($user);
-        $em->flush();
         $this->addFlash('login', 'Félicitations ! Ton inscription est terminée, 
                                                 tu peux maintenant te connecter :)');
         return $this->redirectToRoute('home');
-    }
-
-    public function sendConfirmation(\Swift_Mailer $mailer, $user, $validationToken)
-    {
-        $message = (new \Swift_Message())
-            ->setSubject('Snow Tricks : validation de votre compte')
-            ->setFrom('ulm_ulm@hotmail.com')
-            ->setTo($user->getEmail())
-            ->setBody(
-                $this->renderView(
-                    'emails/registrationValidation.html.twig',
-                    array(
-                          'token' => $validationToken,
-                          'user'  => $user)
-                ),
-                'text/html'
-            );
-        $mailer->send($message);
     }
 }
--- src/Form/Type/UserRegistrationType.php
+++ src/Form/Type/UserRegistrationType.php
@@ -17,7 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class UserType extends AbstractType
+class UserRegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
